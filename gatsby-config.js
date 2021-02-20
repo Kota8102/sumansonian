a49a393796fe8f5b67621f88b57532275f555ab4
mk <pontan0801.konta@gmail.com>
2021-02-20 10:52:36 +0900

    siteMetdate

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Casper',
-    description: 'A port of the casper blog built for gatsby',
-    siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
+    title: 'Sumansonian',
+    description: '技術的内容メイン',
+    siteUrl: 'http://sumansonian.jp/', // full path to blog - no ending slash
   },
   mapping: {
     'MarkdownRemark.frontmatter.author': 'AuthorYaml',
